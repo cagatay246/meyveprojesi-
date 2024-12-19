81d397ef48d12842c28b6d52147f5ff1d9f13d2c
cagatay246 <cagataykilgi7@gmail.com>
2024-12-19 12:30:10 +0300

    meyve
    if-else ve switchle yapılan program

---
+++ if-else/ConsoleApp10/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elma 2tl");
+            Console.WriteLine("armut 3tl");
+            Console.WriteLine("çilek 2tl");
+            Console.WriteLine("muz 3tl");
+            Console.WriteLine("diğer meyveler 4tl");
+            Console.WriteLine("hangi meyveyi almak istersiniz");
+
+
+            //kullanıcıya soru sorma
+            Console.WriteLine("lütfen almak istediğiniz meyveyi girin:");
+            String meyve = Console.ReadLine();
+
+            //meyve ismini küçüf harf yapma
+            meyve = meyve.ToLower();
+
+            //fiyat belirlemek için yapılan if yapısı 
+            if (meyve == "elma")
+            {
+                Console.WriteLine("Elmanın fiyatı:2 tl");
+
+            }
+            else if (meyve == "armut")
+
+            {
+                Console.WriteLine("armutun fiyatı :3 tl");
+
+            }
+            else if (meyve == "çilek")
+            {
+                Console.WriteLine("çileğin fiyatı: 2 tl");
+
+            }
+            else if (meyve == "muz")
+            {
+                Console.WriteLine("muzun fiyatı: 3tl");
+            }
+            else
+            {
+                Console.WriteLine("Diğer meyve fiyatı: 4 TL");
+            }
+
+
+        }
+    }
+}
+
---
+++ switch/switch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Elma fiyatı: 2 TL");
+            Console.WriteLine("Armut fiyatı: 3 TL");
+            Console.WriteLine("Çilek fiyatı: 2 TL");
+            Console.WriteLine("Muz fiyatı: 3 TL");
+            Console.WriteLine("Diğer meyve fiyatı: 4 TL");
+
+
+            Console.WriteLine("almak istediğiniz meyve:");
+            string meyve = Console.ReadLine();
+
+            //kullanıcıdan gelen meyve ismini küçük harf yapmak 
+            meyve = meyve.ToLower();
+
+            switch (meyve)
+            {
+                case "elma":
+                    Console.WriteLine("elma fiyatı 2 tl");
+                    break;
+
+                case "armut":
+                    Console.WriteLine("armut fiyatı 3 tl");
+                    break;
+
+                case "çilek":
+                    Console.WriteLine("çilek fiyatı 2 tl");
+                    break;
+
+                case "muz":
+                    Console.WriteLine("muz fiyatı 3 tl");
+                    break;
+
+                deafult:
+                    Console.WriteLine("diğer meyvelerin fiyatı 4 tl");
+                    break;
+
+            }
+        }
+    }
+}
